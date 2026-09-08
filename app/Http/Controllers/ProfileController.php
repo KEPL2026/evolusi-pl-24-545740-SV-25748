5569a5bcf97b112ae3411ba346a90d85c9a09bfd
muhammadariefand <muhammadariefandriansyah2005@mail.ugm.ac.id>
2026-09-08 15:02:55 +0700

    style: fix code formatting with laravel pint

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,17 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class ProfileController extends Controller
 {
     public function index(): View
     {
         return view('profile', [
-            'name'      => 'Muhammad Arief Andriansyah',
-            'nim'       => '24/545740/SV25748',
-            'program'   => 'Sarjana Terapan (Vokasi)',
-            'university'=> 'Universitas Gadjah Mada',
+            'name' => 'Muhammad Arief Andriansyah',
+            'nim' => '24/545740/SV25748',
+            'program' => 'Sarjana Terapan (Vokasi)',
+            'university' => 'Universitas Gadjah Mada',
         ]);
     }
 }
