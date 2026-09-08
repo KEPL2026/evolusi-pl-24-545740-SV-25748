9ccd8bafe04a2702662c4ea8bbae35c08d583d2f
muhammadariefand <muhammadariefandriansyah2005@mail.ugm.ac.id>
2026-09-08 13:13:41 +0700

    feat: add profile blade view

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Profil Mahasiswa</title>
+</head>
+<body>
+    <h1>{{ $name }}</h1>
+    <p>NIM: {{ $nim }}</p>
+    <p>Program: {{ $program }}</p>
+    <p>Universitas: {{ $university }}</p>
+</body>
+</html>
